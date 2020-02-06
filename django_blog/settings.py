2e98f103c72744be700508317f9b9ec3a1b7d1eb
thisisziihee <iamthejiheee@naver.com>
2020-02-06 14:23:42 +0900

    add secret key

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -1,7 +1,26 @@
 import os
+import json
+from django.core.exceptions import ImproperlyConfigured
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+secret_file = os.path.join(BASE_DIR, "secrets.json")
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        print(secrets[setting])
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {0} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,6 +50,8 @@ INSTALLED_APPS += [
     'user',
     'post',
     'helpers',
+    'taggit',
+    'django_social_share',
 ]
 
 MIDDLEWARE = [
